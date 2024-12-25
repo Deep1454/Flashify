0c85a2a2b5c398241936a3cc8b152a261711aa8b
jemish kevadiya <jemishkevadiya007@gmail.com>
2024-12-24 20:34:16 -0500

    Deleted SignInScreen.js and added logics within SignUpScreen.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import SignUpScreen from '../screens/SignUpScreen';
-import SignInScreen from '../screens/SignInScreen';
 import HomeScreen from '../screens/HomeScreen';
 const Stack = createStackNavigator();
 
